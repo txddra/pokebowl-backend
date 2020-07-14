df5fd60373f02b690024a3ac10c0b2cda874501e
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-07-14 15:05:37 -0400

    got null to work

--- get-url.js
+++ get-url.js
@@ -1,8 +1,22 @@
 // Your code here.
 // Note that you do NOT need the data for this function!
 
+const pokemon = require("./pokemon");
 
+//creating a function that only returns the  url portion of the array
 
+const getUrl = function(pokemonName){
+  for(const row of pokemon){  
+    // console.log(row[1])
+if(row === pokemon){
+
+  return row[1]
+
+}
+  }
+return null
+}
+// const pokeRow 
 
 // OUR code here.
 
